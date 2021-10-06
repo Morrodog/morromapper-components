e805c784474ab89e50563599bef4ab8f77c2b92a
Josh Randall <josh-randall@hush.ai>
2021-10-05 23:42:22 -0400

    Fix exports

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import MMMap        from '/src/components/mm-map.vue'
 
 import 'font-awesome/css/font-awesome.min.css'
 
-export MMMap MMMap;
-export MMMDialog MMMDialog;
-export MMPopup MMPopup;
-export MMPopup MMPopup;
-export MMBlob MMBlob;
-export MMCellpicker MMCellpicker;
+export MMMap
+export MMMDialog
+export MMPopup
+export MMPopup
+export MMBlob
+export MMCellpicker
