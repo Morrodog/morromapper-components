7da086d58756be115b354ad22521b7a820ecd384
Josh Randall <josh-randall@hush.ai>
2021-10-05 23:48:57 -0400

    Fix exports

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import MMCellpicker from '/src/components/mm-cellpicker.vue'
-import MMDialog     from '/src/components/mm-dialog.vue'
-import MMPopup      from '/src/components/mm-popup.vue'
-import MMBlob       from '/src/components/mm-blob.vue'
-import MMMap        from '/src/components/mm-map.vue'
+import _MMCellpicker from '/src/components/mm-cellpicker.vue'
+import _MMDialog     from '/src/components/mm-dialog.vue'
+import _MMPopup      from '/src/components/mm-popup.vue'
+import _MMBlob       from '/src/components/mm-blob.vue'
+import _MMMap        from '/src/components/mm-map.vue'
 
 import 'font-awesome/css/font-awesome.min.css'
 
-export const MMCellpicker = MMCellpicker;
-export const MMDialog = MMDialog;
-export const MMPopup = MMPopup;
-export const MMBlob = MMBlob;
-export const MMMap = MMMap;
+export const MMCellpicker = _MMCellpicker;
+export const MMDialog = _MMDialog;
+export const MMPopup = _MMPopup;
+export const MMBlob = _MMBlob;
+export const MMMap = _MMMap;
